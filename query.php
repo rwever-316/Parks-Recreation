ab8c36547b985b817712086851caadd94cedd7ef
rwever-316 <rwever@asu.edu>
2016-04-29 21:05:50 -0700

    New query.php file to replace prior file.
    All queries modified and working with new tables from data.php

--- query.php
+++ query.php
@@ -1,15 +1,12 @@
 <?php
 /* make connection */
 	$link = mysqli_connect("localhost","root","");
-
 /* check connection */
 	if(!$link){
 		die('Connection Failed: ' . mysqli_error($link));
 	}
-
 /* make & check connection to database*/
 	$db = mysqli_select_db($link,"test3") or die('Unable to selec database');
-
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +26,6 @@
 		position: relative;
 		margin-top: 0px;
 		}
-
 		#footer{
 			position: fixed;
 			bottom: 10px;
@@ -73,9 +69,27 @@
 			echo "No selection made.";
 		}
 
-		else {	
+		else {
+			if (($state=="Arizona")){
+				$state="AZ";
+			}
+			if (($state=="California")){
+				$state="CA";
+			}
+			if (($state=="Illinois")){
+				$state="IL";
+			}
+			if (($state=="New York")){
+				$state="NY";
+			}
+			if (($state=="Texas")){
+				$state="TX";
+			}
+			if (($state=="Wyoming")){
+				$state="WY";
+			}
 /* execute query */
-			$query="SELECT park.park_Name, address.street, address.city, address.state, address.zip_Code FROM park JOIN address WHERE (address.park_ID=park.park_ID) AND (address.state='$state')";
+			$query="SELECT park_Name, park_street, park_city, park_state, park_zip_code FROM park WHERE (park_state='$state')";
 			$result = mysqli_query($link,$query);
 			
 			if (!$result){
@@ -83,15 +97,14 @@
 			}
 			else{
 /* display results */
-		    	echo "All the parks in ".$state." are:";
+		    	echo "All the parks in ".($_POST['state'])." are:";
 				echo nl2br("\n\n");
-
 		    	while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
 			    	echo $row['park_Name'];
 				    echo nl2br("\n");
-				    echo $row['street'];
+				    echo $row['park_street'];
 				    echo nl2br("\n");
-				    echo $row['city'].', '.  $row['state']. ' '.$row['zip_Code'];
+				    echo $row['park_city'].', '.  $row['park_state']. ' '.$row['park_zip_code'];
 				    echo nl2br("\n\n");
 				}
 /* free result set */
@@ -99,35 +112,48 @@
 			}
 		}
 	}
-
-
-
-
 /* *----------------------------------QUERY 2------------------------------------*/
 		if (isset($_POST['park'])){
-		$park=$_POST['park'];
-		/*echo "park SELECTION is: " . $park;	
-		echo nl2br("\n");*/
-
+			$park=$_POST['park'];
+		
 		if (isset($_POST['difficulty'])){
 			$difficulty=$_POST['difficulty'];
-			/*	echo "difficulty SELECTION is: " . $difficulty;	
-			echo nl2br("\n");*/
 		}
 
-/* execute query */
-    $query="SELECT trail_Name, street, city, state, zip_Code, park_Name, difficulty FROM park JOIN trail, address WHERE (trail.park_ID=park.park_ID) AND (address.park_ID=park.park_ID)";
-    $parkOutputText = ":";
-    if (!($park=="")) {
-      $query = $query . "AND (park.park_Name='$park')";
-      $parkOutputText=" in ".$park." are:";
-    }
-    if (!($difficulty=="")) {
-      $query = $query . "AND (trail.difficulty='$difficulty')";
-    }
-		//$query="SELECT trail_Name FROM park JOIN trail WHERE (trail.park_ID=park.park_ID) AND (park.park_Name='$park') AND (trail.difficulty='$difficulty')";
-		$result = mysqli_query($link,$query);
-		
+
+		if ($park==""){
+			if($difficulty==""){
+				$query="SELECT trail_Name,park_Name, park_street, park_city, park_state, park_zip_code FROM park, trail WHERE (park.park_Name='$park') AND (trail.difficulty='$difficulty')";
+				$result = mysqli_query($link,$query);
+				echo "No selection made.";
+			}
+		}
+
+		if (!($park=="")){
+			if($difficulty==""){
+				$query="SELECT trail_Name, park_Name, park_street, park_city, park_state, park_zip_code, difficulty FROM park, trail WHERE (trail.park_ID=park.park_ID) AND (park_Name='$park')";
+				echo "Only the park was selection was made.";
+				echo nl2br("\n");
+				$result = mysqli_query($link,$query);
+			}
+		}
+
+		if(!($difficulty=="")){
+			if(($_POST['park']=="")){
+				$query="SELECT trail_Name, park_Name,park_street, park_city, park_state, park_zip_code FROM park, trail WHERE (trail.park_ID=park.park_ID) AND (difficulty='$difficulty')";
+				echo "Only the difficulty level selection was made.";
+				echo nl2br("\n");
+				$result = mysqli_query($link,$query);
+			}
+		}
+
+		if (!($park=="")){
+			if(!($difficulty=="")){
+				$query="SELECT trail_Name,park_Name, park_street, park_city, park_state, park_zip_code FROM park, trail WHERE (park.park_Name='$park') AND (trail.difficulty='$difficulty')";
+				$result = mysqli_query($link,$query);
+			}
+		}
+
     $difficultyString = "";
     switch($difficulty) {
       case 1:
@@ -142,190 +168,159 @@
       default:
         break;
     }
-		if (!$result){
-			echo 'Error fetching results: '.mysqli_error($link);
-		}
-    elseif (mysqli_num_rows($result) == 0){
-      echo "No results.";
-    }
-		else{
+   
+		if (!($_POST['park']=="")){
+			if(!($_POST['difficulty']=="")){
+    			if (mysqli_num_rows($result) == 0){
+      				echo "No results.";
+    			}
+    		}
+    	}
+    	if (mysqli_num_rows($result) > 0){
+      				
 /* display results */
-	    	echo "All ".$difficultyString." trails" . $parkOutputText;
+	    	echo "All of the ".$difficultyString." trails";
+	    	if (!($park=="")){
+		    	echo " in " . $park;
+		    }
+		    echo " are:";
 			echo nl2br("\n\n");
-
 	    	while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
 		    	echo $row['trail_Name'];
-          echo nl2br("\n");
-          if (($park=="")){
-            echo $row['park_Name'];
-            echo nl2br("\n");
-          }
-			    echo $row['street'];
-			    echo nl2br("\n");
-			    echo $row['city'].', '.  $row['state']. ' '.$row['zip_Code'];
-			    echo nl2br("\n");
-          if (($difficulty=="")){
-            switch ($row['difficulty']) {
-              case '1':
-                echo "Difficulty: Easy";
-                break;
-              case '2':
-                echo "Difficulty: Moderate";
-                break;              
-              case '3':
-                echo "Difficulty: Difficult";
-                break;                
-              default:
-                echo "Error finding difficulty";
-                break;
-            }
-          }
-          echo nl2br("\n\n");          
+         		echo nl2br("\n");
+         
+          		if (($park=="")){
+          			if (!($difficulty=="")){
+            			echo $row['park_Name'];
+            			echo nl2br("\n");
+			        }
+			    }
+			    	echo $row['park_street'];
+			    	echo nl2br("\n");
+			    	echo $row['park_city'].', '.  $row['park_state']. ' '.$row['park_zip_code'];
+			    	echo nl2br("\n");
+          
+	          		if (($difficulty=="")){
+	            		switch ($row['difficulty']) {
+	              			case '1':
+	                			echo "Difficulty: Easy";
+	                			break;
+	              			case '2':
+	                			echo "Difficulty: Moderate";
+	                			break;              
+	              			case '3':
+	                			echo "Difficulty: Difficult";
+	                			break;                
+	              			default:
+	                			echo "Error finding difficulty";
+	                			break;
+	            		}
+	        		}
+          		/*}*/
+          		echo nl2br("\n\n");          
 		    }
-		/* free result set */
-	mysqli_free_result($result);
+/* free result set */
+		mysqli_free_result($result);
+		}
 	}
-}
-
-
 /* *----------------------------------QUERY 3------------------------------------*/
      if (isset($_POST['park2'])){
-        $park=$_POST['park2'];
+        $park2=$_POST['park2'];
 /*    echo "SELECTION is: " . $park;    */
 
 /* check submit value */
-	    if (($park=="")){
+	    if (($park2=="")){
 	        echo "No selection made for camps.";
 	    }
-
 	    else {
 /* execute query */
-	        $query="SELECT camp_Name, hiking, biking, swimming, fishing, boating, kayaking,
-	                winter_sports, pets_Allowed, wheelchair_Access, rv_Sites, tent_Sites
-	                FROM camp, activity, park
-	                WHERE camp.camp_ID = activity.camp_ID
-	                AND park.park_ID = camp.park_ID
-	                AND park.park_Name = '$park'";
-
+	        $query="SELECT activity, park_Name, camp_Name FROM camp, activity, park WHERE ((camp.camp_ID = activity.camp_ID) AND (park.park_ID = camp.park_ID) AND (park.park_Name = '$park2'))";
 	        $result = mysqli_query($link,$query);
-
 	        if (!$result){
 	            echo 'Error fetching results: '.mysqli_error($link);
 	        }
 	        else{
-	            /* display results */
-	            echo "All the activities in ".$park." are:";
-	            echo nl2br("\n\n");
-
-	            while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
-	            	echo "Camp ". $row['camp_Name'] . " includes:";
-	                if($row['hiking'] == 1)
-	                  echo nl2br("\n") ."hiking ";
-	                if($row['biking'] == 1)
-	                  echo nl2br("\n") . "biking ";
-	                if($row['swimming'] == 1)
-	                  echo nl2br("\n") . "swimming ";
-	                if($row['fishing'] == 1)
-	                  echo nl2br("\n") . "fishing ";
-	                if($row['boating'] == 1)
-	                  echo nl2br("\n") . "boating ";
-	                if($row['kayaking'] == 1)
-	                  echo nl2br("\n") . "kayaking ";
-	                if($row['winter_sports'] == 1)
-	                  echo nl2br("\n") . "winter sports ";
-	                if($row['pets_Allowed'] == 1)
-	                  echo nl2br("\n") . "pets allowed ";
-	                if($row['wheelchair_Access'] == 1)
-	                  echo nl2br("\n") . "wheelchair access ";
-	                if($row['rv_Sites'] == 1)
-	                  echo nl2br("\n") . "RV sites ";
-	                if($row['tent_Sites'] == 1)
-	                  echo nl2br("\n") . "tent sites ";
+/* display results */
+  			   	echo nl2br("\n");
+  				echo $park2." has camps with the following activities and/or facilities...";
+		           echo nl2br("\n\n");
+   		    	while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
+	  			    echo $row['camp_Name'] . ":";
+	  			   	echo nl2br("\n");
+	  			    echo $row['activity'];
 	                echo nl2br("\n\n");
+}
 	            }
 /* free result set */
     			mysqli_free_result($result);
         	}
     	}
-	}
-
+	
 /* *----------------------------------QUERY 4------------------------------------*/
 if (isset($_POST['region'])){
-        $region=$_POST['region'];
+    $region=$_POST['region'];
+    $query="SELECT trail_Name, trail_Type, park_Name, park_street, park_city, park_state, park_zip_code, region FROM park, trail WHERE trail.park_ID = park.park_ID";
+    if (!($region=="")){
+    	$query = $query . " AND (region = '$region')";
+    }
+/* execute query */
+    $result = mysqli_query($link,$query);
+    if (!$result){
+    	echo 'Error fetching results: '.mysqli_error($link);
+    }
+    else{
+/* display results */
+	    if (mysqli_num_rows($result) == 0){
+	    	echo "No results found.";
+	    }
+	    if (($region=="")){
+	        echo "All trails:";
+	        echo nl2br("\n");
+	    }
+	    if (!($region=="")){
+	        echo "All trails in the $region region:";             
+	        echo nl2br("\n\n");
+		}
+	    while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
+		    echo $row['trail_Name']. " located in ". $row['park_Name'];
+		    echo nl2br("\n");
+		    echo $row['park_street'];
+		    echo nl2br("\n");
+		    echo $row['park_city'].', '.  $row['park_state']. ' '.$row['park_zip_code'];
+		    echo nl2br("\n");
+		    echo 'Trail Type: ' . $row['trail_Type'];
+		    echo nl2br("\n");
 
-      $query="SELECT trail_Name, trail_Type, street, city, state, zip_Code, region
-              FROM park, trail, address
-              WHERE park.park_ID = address.park_ID
-              AND trail.park_ID = park.park_ID";
+	        if (($region=="")){
+	    	    echo "Region: ". $row['region'];
+	        }          
+	        echo nl2br("\n\n");
+	    }
+    }
 
-        if (!($region=="")){
-          $query = $query . " AND (region = '$region')";
-        }
-
-      /* execute query */
-
-      $result = mysqli_query($link,$query);
-
-      if (!$result){
-          echo 'Error fetching results: '.mysqli_error($link);
-      }
-      else{
-        /* display results */
-        if (($region=="")){
-          echo "All trails:";
-        }
-        else {
-          echo "All trails in the $region region:";         
-        }
-        echo nl2br("\n\n");
-
-        while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
-          echo $row['trail_Name'];
-          echo nl2br("\n");
-          echo 'Trail Type: ' . $row['trail_Type'];
-          echo nl2br("\n");
-          echo $row['street'];
-          echo nl2br("\n");
-          echo $row['city'].', '.  $row['state']. ' '.$row['zip_Code'];
-          echo nl2br("\n");
-          if (($region=="")){
-            echo "Region: ". $row['region'];
-          }          
-          echo nl2br("\n\n");
-        }
-        /* free result set */
-    mysqli_free_result($result);
-        }
-
+/* free result set */
+    mysqli_free_result($result);       
 }
-
 /* *----------------------------------QUERY 5------------------------------------*/
     if (isset($_POST['submitquery5'])){
-
 /* execute query */
-      	$query="SELECT camp_Name, street, city, state, zip_Code, rating
-            FROM camp, address
-            WHERE camp.park_ID = address.park_ID
-           	AND rating > 3
-            ORDER BY rating DESC LIMIT 5";
-
+		$query="SELECT C3.park_ID, C3.camp_Name, C4.park_Name, C4.park_street, C4.park_city, C4.park_state, C4.park_zip_code, C3.rating FROM (SELECT rating, camp_Name, park_ID FROM camp c1 WHERE NOT EXISTS (SELECT * FROM camp C2 WHERE C2.rating > C1.rating)) C3, park C4 WHERE (C4.park_ID = C3.park_ID)";
+		/*
+      	$query="SELECT C1.camp_Name, park_Name, park_street, park_city, park_state, park_city, park_zip_code FROM park, camp C1, camp C2 WHERE NOT EXISTS (SELECT * FROM camp C2 WHERE ((C2.rating > C1.rating)AND (park.park_ID = C1.park_ID)) ORDER BY (C1.camp_Name)"; */
       	$result = mysqli_query($link,$query);
-
 	    if (!$result){
 	    	echo 'Error fetching results: '.mysqli_error($link);
 	    }
-
       	else{
 /* display results */
 	        echo "The Top Rated camps are:";
 	        echo nl2br("\n\n");
-
 	        while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
 		        echo $row['camp_Name'];
 		        echo nl2br("\n");
-		        echo $row['street'];
+		        echo $row['park_street'];
 		        echo nl2br("\n");
-		        echo $row['city'].', '.  $row['state']. ' '.$row['zip_Code'];
+		        echo $row['park_city'].', '.  $row['park_state']. ' '.$row['park_zip_code'];
 		        echo nl2br("\n");
 		        echo 'rating: ' . $row['rating'];
 		        echo nl2br("\n\n");
@@ -333,10 +328,8 @@ if (isset($_POST['region'])){
 /* free result set */
     	mysqli_free_result($result);
         }
-
 	}
 /* *----------------------------------END OF QUERIES------------------------------------*/
-
 /* close connection */
 	mysqli_close($link);
 ?>
